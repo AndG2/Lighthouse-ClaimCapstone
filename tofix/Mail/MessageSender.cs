995abcd867b90175cc4063be384141f0fb4af530
AndG2 <81577067+AndG2@users.noreply.github.com>
2021-04-26 18:19:20 -0500

    added 80% of email signup response, remember to check notes and adjust later

---
+++ tofix/Mail/MessageSender.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Net;
+using System.Net.Mail;
+using System.Web;
+
+namespace tofix.Mail
+{
+    public static class MessageSender
+    {
+        private static readonly string _smtpHost = 
+            ConfigurationManager.AppSettings["SmtpHost"];
+        private static readonly int _smtpPort = int.Parse(ConfigurationManager.AppSettings["SmtpPort"]);
+        private static readonly string _smtpUsername = ConfigurationManager.AppSettings["SmtpUsername"];
+        private static readonly string _smtpPassword = ConfigurationManager.AppSettings["SmtpPassword"];
+        private static readonly string _fromAddress = ConfigurationManager.AppSettings["FromAddress"];
+
+        public static void SendEmail(string toAddress, string subject, string messageBody)
+        {
+            using(var client = new SmtpClient(_smtpHost, _smtpPort))
+            {
+                client.EnableSsl = true;
+                client.UseDefaultCredentials = false;
+                client.Credentials = new NetworkCredential(_smtpUsername,_smtpPassword);
+
+                using (var msg = new MailMessage())
+                {
+                    msg.From = new MailAddress(_fromAddress);
+                    msg.To.Add(toAddress);
+                    msg.Subject = subject;
+                    msg.Body = messageBody;
+
+                    client.Send(msg);
+                }
+            }
+        }
+
+    }
+}

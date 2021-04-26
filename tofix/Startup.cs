c35a731f247fd4596d704077031daf4937e9de8b
AndG2 <81577067+AndG2@users.noreply.github.com>
2021-04-26 13:59:31 -0500

    Add project files.

---
+++ tofix/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(tofix.Startup))]
+namespace tofix
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
